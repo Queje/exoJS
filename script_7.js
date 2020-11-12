dbd043556225fd4abf3ff83d2185a72259bdbcec
Queje <jeremyquerne@hotmail.fr>
2020-11-12 22:21:51 +0100

    finished the bot

--- script_7.js
+++ script_7.js
@@ -4,14 +4,17 @@ function acneBot(){
     if (starter.endsWith("?",starter.length)){
         console.log("ouais, ouais....");
     }
-    else if (starter == starter.toUpperCase()) {
+    else if (starter == starter.toUpperCase() && starter.length !== 0) {
         console.log("Pwa, calme-toi...");
     }
     else if (starter.indexOf('fortnite') > -1) {
         console.log("on s' fait une partie soum-soum ?"); 
     }
     else if (starter.length === 0) {
-        
+        console.log("t'es en PLS ?")
+    }
+    else {
+        console.log("balek")
     }
 }
 
