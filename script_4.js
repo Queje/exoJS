47c2ad73269c24478f5845ed0a6de21390f8160a
Queje <jeremyquerne@hotmail.fr>
2020-11-12 18:41:33 +0100

    2nd commit after startup nation exercice

--- script_4.js
+++ script_4.js
@@ -18,4 +18,57 @@ const entrepreneurs = [
     { first: 'Marc', last: 'Andreessen', year: 1971 },
     { first: 'Peter', last: 'Thiel', year: 1967 }
   ];
+
+  console.log("les entrepreneurs qui sont nés dans les années 70 sont:");
+
+  entrepreneurs.forEach(index => {
+    if (index.year >= 1970 && index.year <= 1979) {
+        console.log(index);
+    }
+  });
+
+
+
+
+  console.log("une array qui contient le prénom et le nom des entrepreneurs:");
+
+  var copie = [];
+
+  entrepreneurs.forEach(function(index){
+    copie.push({first: index.first, last: index.last});
+});
+  console.log(copie);
+
+
+
+
+  console.log("un array qui contient l'age des entrepreneurs (si ils étaient tous vivants):");
+
+  var copie2 = [];
+
+  entrepreneurs.forEach(function(index){
+    copie2.push({age: 2020 - index.year});
+});
+  console.log(copie2);
+
+
+
+
+  console.log("un array trier par ordre alphabetic des noms de famille:");
+
+  entrepreneurs.sort(function compare(a, b) {
+    if (a.last < b.last)
+       return -1;
+    if (a.last > b.last )
+       return 1;
+    return 0;
+  });
+  console.log(entrepreneurs);
+
+
+
+
+  
+ 
+   
   
